353a07fb516548b1edf160de22604af27ae8600f
Nicolas Maas <nicolas.maas@student.howest.be>
2019-11-12 14:07:12 +0100

    Init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div class="o-container">
+            <div class="o-row"></div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "home",
+
+    components: {},
+
+    beforeCreate() {
+        // Change Standard Page Title
+        document.title = "Labo - Todoist";
+    }
+};
 </script>
